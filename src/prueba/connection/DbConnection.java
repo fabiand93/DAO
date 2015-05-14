15be672a883ac8f2287e0d5c01bbf6cda056861d
Fabian <fabian@MacBook-Air-de-Fabian.local>
2015-05-14 09:13:37 -0500

    Patron DAO
    Crea y consulta empleados en una base de datos

---
+++ src/prueba/connection/DbConnection.java
@@ -0,0 +1,43 @@
+package prueba.connection;
+import java.sql.*;
+
+/**
+ * Clase que permite conectar con la base de datos
+ * @author Fabian
+ *
+ */
+public class DbConnection {
+static String bd = "empleados";
+static String login = "root";
+static String password = "";
+static String url = "jdbc:mysql://localhost/"+bd;
+Connection connection = null;
+
+public DbConnection() {
+try{
+//obtenemos el driver de para mysql
+Class.forName("com.mysql.jdbc.Driver");
+connection = DriverManager.getConnection(url,login,password);
+if (connection!=null){
+		System.out.println("Conexión a base de datos "+bd+" OK\n");
+		}
+}
+	catch(SQLException e){
+	System.out.println(e);
+	}
+catch(ClassNotFoundException e){
+	System.out.println(e);
+	}
+	catch(Exception e){
+		System.out.println(e);
+		}
+}
+/**Permite retornar la conexión*/
+public Connection getConnection(){
+	return connection;
+	}
+
+public void desconectar(){
+	connection = null;
+	}
+}
